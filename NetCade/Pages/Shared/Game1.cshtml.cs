ab5a144df06023e91e8e29d471b58398b497c0b1
GREGORY PACHECO <GREPACHE@uat.edu>
2020-02-06 14:33:27 -0700

    Game 1

---
+++ NetCade/Pages/Shared/Game1.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NetCade.Pages.Shared
+{
+    public class Game1Model : PageModel
+    {
+        public void OnGet()
+        {
+            //This is where the code for game 1 will go
+        }
+    }
+}
