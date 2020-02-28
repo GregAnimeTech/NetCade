aa0e6607b654c37a9408246bbe81582c70b55445
GREGORY PACHECO <GREPACHE@uat.edu>
2020-02-28 15:21:32 -0700

    Cool changes to page

--- NetCade/Pages/StoryGame.cshtml.cs
+++ NetCade/Pages/StoryGame.cshtml.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-
 namespace NetCade.Pages
 {
     public class StoryGameModel : PageModel
