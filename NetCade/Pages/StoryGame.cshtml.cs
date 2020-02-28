e543dee55da15d15ea8284c943622073ea37542e
GREGORY PACHECO <GREPACHE@uat.edu>
2020-02-28 14:16:41 -0700

    no story temp

--- NetCade/Pages/StoryGame.cshtml.cs
+++ NetCade/Pages/StoryGame.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
 namespace NetCade.Pages
 {
     public class StoryGameModel : PageModel
