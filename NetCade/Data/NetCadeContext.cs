bf9825e8ab7af8d41353aea2f7a2248f261c8ce7
GREGORY PACHECO <GREPACHE@uat.edu>
2020-02-28 17:03:13 -0700

    Start of a database

---
+++ NetCade/Data/NetCadeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetCade.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCade.Data
+{
+    public class NetCadeContext : DbContext
+    {
+        public DbSet<UserAccount> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source=Bakery.db");
+        }
+    }
+}
--- NetCade/Startup.cs
+++ NetCade/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NetCade.Data;
 
 namespace NetCade
 {
@@ -24,6 +25,7 @@ namespace NetCade
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<NetCadeContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
