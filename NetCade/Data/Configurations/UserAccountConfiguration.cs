56bfb9701853bc0e7962a7aef8b0f292a5b4a408
GREGORY PACHECO <GREPACHE@uat.edu>
2020-03-01 19:05:53 -0700

    Before the migration

---
+++ NetCade/Data/Configurations/UserAccountConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetCade.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NetCade.Data.Configurations
+{
+    public class UserAccountConfiguration : IEntityTypeConfiguration<UserAccount>
+    {
+        public void Configure(EntityTypeBuilder<UserAccount> builder)
+        {
+            builder.Property(p => p.UserName).HasColumnName("UserAccountName");
+        }
+    }
+}
--- NetCade/Data/NetCadeContext.cs
+++ NetCade/Data/NetCadeContext.cs
@@ -4,16 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using NetCade.Models;
 using Microsoft.EntityFrameworkCore;
+using NetCade.Data.Configurations;
 
 namespace NetCade.Data
 {
     public class NetCadeContext : DbContext
     {
-        public DbSet<UserAccount> Accounts { get; set; }
+        public DbSet<UserAccount> UserAccounts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data source=Bakery.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserAccountConfiguration()).Seed();
         }
     }
 }
