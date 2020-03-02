7bfc0a64343aae5d7270bdd60536789d8f5e26fe
GREGORY PACHECO <GREPACHE@uat.edu>
2020-03-01 20:58:55 -0700

    game database done (with testing)

--- NetCade/Pages/Index.cshtml.cs
+++ NetCade/Pages/Index.cshtml.cs
@@ -5,12 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using NetCade.Data;
+using NetCade.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace NetCade.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        /*For game database*/
+        //private readonly NetCadeContext db;
+        //public IndexModel(NetCadeContext db) => this.db = db;
+        //public List<UserAccount> UserAccount { get; set; } = new List<UserAccount>();
+        //public UserAccount FeaturedUserName { get; set; }
+        //public async Task OnGetAsync()
+        //{
+        //    UserAccount = await db.UserAccounts.ToListAsync();
+        //    FeaturedUserName = UserAccount.ElementAt(new Random().Next(UserAccount.Count));
+        //}
 
         public IndexModel(ILogger<IndexModel> logger)
         {
