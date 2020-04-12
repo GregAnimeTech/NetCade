362bc82c402dc882f0033ed18e1e9c82a05e36c9
GREGORY PACHECO <GREPACHE@uat.edu>
2020-04-11 23:45:57 -0700

    Got both games on the site, just as downloadable links for now

--- NetCade/Pages/Game1.cshtml.cs
+++ NetCade/Pages/Game1.cshtml.cs
@@ -2,28 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Web;
 
 namespace NetCade.Pages
 {
     public class Game1Model : PageModel
     {
-        [BindProperty]
         public string Time { get; set; }
         public void OnGet()
         {
             Time = DateTime.Now.ToShortTimeString();
         }
-        //public void Link_Click()
-        //{
-        //    Response.ContentType = "application/octect-stream";
-        //    Response.AppendHeader("content-disposition", "filename=SEP Game 1.exe");
-        //    Response.TransmitFile(Server.MapPath("~/JellyRunBuild/Sep Game 1.exe"));
-        //    Response.End();
-        //}
-
     }
 }
